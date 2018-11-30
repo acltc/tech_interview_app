69a1594c31db61bfd4462f0835aadd593fa6789a
Jay Wengrow <jaywngrw@gmail.com>
2018-11-30 14:10:25 -0600

    added a lot more content

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'pages#index'
   get '/basic-algorithms' => 'pages#basic_algorithms'
+  get '/whiteboarding' => 'pages#whiteboarding'
+  get '/exercism' => 'pages#exercism'
+  get '/coding-interview-platforms' => 'pages#coding_interview_platforms'
+  get '/architecture' => 'pages#architecture'
+  get '/homework' => 'pages#homework'
+
 end
